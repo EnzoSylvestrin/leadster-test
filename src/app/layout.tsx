3b62a23bd5ef119b4765a49a9d5148b73975b079
EnzoSylvestrin <enzospavani@gmail.com>
2023-06-25 19:14:47 -0300

    fix(layout): taking out metadata on 'use client' component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,11 +7,6 @@ import { Container } from '@/styles/globalStyles/Main';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 
-export const metadata = {
-  title: 'Leadster test',
-  description: 'site made to leadster test',
-}
-
 export const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="pt">

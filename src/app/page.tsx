a584b5987c1b71c646382d6b4ded59e88c52e9c4
EnzoSylvestrin <enzospavani@gmail.com>
2023-06-25 14:05:24 -0300

    feat(videos): adding functionality to filters and pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,14 @@ import Asset from '@/assets/elements/asset-header.png';
 import SeloRD from '@/assets/elements/selo_RD.png';
 import Rating from '@/assets/elements/rating.webp';
 
-import { PaginationNumber } from '@/components/PaginationNumbers/PaginationNumbers';
-import { FilterCard } from '@/components/FilterCard/FilterStyled';
 import { TextStyled } from "@/components/Text/TextStyled";
 import { Button } from '@/components/Button/ButtonStyled';
-import { Select } from '@/components/Select/SelectStyled';
 import { Line } from "@/components/Line/LineStyled";
 
-import { ContainerVideos, WrapperCards, WrapperFilters, WrapperPagination, WrapperSelect, WrapperVideos } from '@/styles/initialPage/VideosStyled';
 import { ContainerAttention, ContainerPresentation, Holder, ImageStyled, TextConvertion, LinePresentation, TextChat } from "@/styles/initialPage/PresentationStyled";
 import { ContainerDemonstration, Separator } from '@/styles/initialPage/DemonstrationStyled';
 import { SimpleImage, Wrapper } from '@/styles/globalStyles/Commom';
+import VideosSection from '@/components/Videos/Videos';
 
 const Home = () => {
   return (
@@ -33,38 +30,7 @@ const Home = () => {
         <LinePresentation />
         <TextStyled size="md" align='center' style={{ margin: '12px 10px' }}>Conheça as estratégias que <span style={{ fontWeight: 700 }}>mudaram o jogo</span> e como aplicá-las no seu negócio</TextStyled>
       </ContainerPresentation>
-      <ContainerVideos>
-        <WrapperVideos direction="column" align="center" justify='center' width="80%">
-          <WrapperFilters align="center" justify='space-between' width='100%'>
-            <WrapperCards width='auto' align="center" justify="center" gap="8px">
-              <FilterCard>Agências</FilterCard>
-              <FilterCard>ChatBot</FilterCard>
-              <FilterCard>Marketing Digital</FilterCard>
-              <FilterCard className='active'>Geração de Leads</FilterCard>
-              <FilterCard>Mídia paga</FilterCard>
-            </WrapperCards>
-            <WrapperSelect width='auto' align="center" justify="center" gap='8px'>
-              <TextStyled align='center' size='1rem'>Ordenar por</TextStyled>
-              <Select> {/* Select é normalmente usado de alguma biblioteca, nesse caso simples usado do browser por não saber as especificações */}
-                <option>Data de publicação</option>
-                <option>Option 1</option>
-                <option>Option 2</option>
-                <option>Option 3</option>
-              </Select>
-            </WrapperSelect>
-          </WrapperFilters>
-          <Line width='100%' margin='20px 0px' />
-          <p>TODO: Videos</p>
-          <Line width='100%' margin='20px 0px' />
-          <WrapperPagination align="center" justify="center" gap="4px">
-            <TextStyled size="1rem" weight='700' style={{ marginRight: '2px' }}>Página</TextStyled>
-            <PaginationNumber>1</PaginationNumber>
-            <PaginationNumber>2</PaginationNumber>
-            <PaginationNumber className='active'>3</PaginationNumber>
-            <PaginationNumber>4</PaginationNumber>
-          </WrapperPagination>
-        </WrapperVideos>
-      </ContainerVideos>
+      <VideosSection />
       <ContainerDemonstration lightBg>
         <Wrapper align='center' justify='center'>
           <SimpleImage src={Comparativo} alt='Leadter results' widthintern='550px' />
---
+++ src/components/Videos/Videos.tsx
@@ -0,0 +1,93 @@
+import { useState, MouseEvent } from 'react';
+
+import { PaginationNumber } from "../PaginationNumbers/PaginationNumbers";
+import { FilterCard } from "../FilterCard/FilterStyled";
+import { TextStyled, } from "../Text/TextStyled";
+import { Select } from "../Select/SelectStyled";
+import { Line } from "../Line/LineStyled";
+
+import { ContainerVideos, WrapperVideos, WrapperFilters, WrapperCards, WrapperSelect, WrapperPagination } from "./VideosStyled";
+
+type CardObject = {
+    name: string,
+}
+
+const VideosSection = () => {
+
+    const [cardId, setCardId] = useState<number>(3);
+    const [paginationId, setPaginationId] = useState<number>(2);
+
+    const FilterCards: CardObject[] = [
+        { name: 'Agências' },
+        { name: 'ChatBot' },
+        { name: 'Marketing Digital' },
+        { name: 'Geração de Leads' },
+        { name: 'Mídia paga' }
+    ]
+
+    const videoArrayLength = 36; //dinamic
+    const videosPerPage = 9;
+    const totalPages = Math.ceil(videoArrayLength / videosPerPage);
+
+    const pageNumbers = Array.from({ length: totalPages }, (_, index) => index + 1);
+
+    const HandleCardClick = (event: MouseEvent<HTMLDivElement>) => {
+        setCardId(parseInt(event.currentTarget.id));
+    }
+
+    const HandlePaginationClick = (event: MouseEvent<HTMLDivElement>) => {
+        setPaginationId(parseInt(event.currentTarget.id));
+    }
+
+    return (
+        <ContainerVideos>
+            <WrapperVideos direction="column" align="center" justify='center' width="80%">
+                <WrapperFilters align="center" justify='space-between' width='100%'>
+                    <WrapperCards width='auto' align="center" justify="center" gap="8px">
+                        {
+                            FilterCards.map((filter, i) => (
+                                <FilterCard
+                                    key={i}
+                                    id={`${i}`}
+                                    className={cardId === i ? 'active' : ''}
+                                    onClick={HandleCardClick}
+                                >
+                                    {filter.name}
+                                </FilterCard>
+                            ))
+                        }
+                    </WrapperCards>
+                    <WrapperSelect width='auto' align="center" justify="center" gap='8px'>
+                        <TextStyled align='center' size='1rem'>Ordenar por</TextStyled>
+                        <Select> {/* Select é normalmente usado de alguma biblioteca, nesse caso simples usado do browser por não saber as especificações */}
+                            <option>Data de publicação</option>
+                            <option>Option 1</option>
+                            <option>Option 2</option>
+                            <option>Option 3</option>
+                        </Select>
+                    </WrapperSelect>
+                </WrapperFilters>
+                <Line width='100%' margin='20px 0px' />
+                <p>TODO: Videos</p>
+                <Line width='100%' margin='20px 0px' />
+                <WrapperPagination align="center" justify="center" gap="4px">
+                    <TextStyled size="1rem" weight='700' style={{ marginRight: '2px' }}>Página</TextStyled>
+                    {
+                        pageNumbers.map((pageNumber, i) => (
+                            <PaginationNumber
+                                key={i}
+                                id={`${i}`}
+                                className={paginationId === i ? 'active' : ''}
+                                onClick={HandlePaginationClick}
+                            >
+                                {pageNumber}
+                            </PaginationNumber>
+                        ))
+                    }
+                </WrapperPagination>
+            </WrapperVideos>
+        </ContainerVideos>
+    );
+}
+
+export default VideosSection;
